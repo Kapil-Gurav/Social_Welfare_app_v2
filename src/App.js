e6eeec8bcbdf70f38f193d751d80927b49b58c72
Kapil-Gurav <160912998+Kapil-Gurav@users.noreply.github.com>
2024-06-19 12:28:21 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { Outlet, Navigate,Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
+import  Login  from './pages/Login';
+import ResetPassword from './pages/ResetPassword';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Register from './pages/Register';
+
+function Layout(){
+ const user = null;
+ const location = useLocation()
+
+ return user?.token ?(
+  <Outlet/>
+ ): (
+  <Navigate to="/login" state={{from: location}} replace/>
+ )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full min-h-[100vh]">
+      <Routes>
+        <Route element={<Layout />}>
+        <Route path='/' element={<Home />} />
+        <Route path='/profile/:id?' element={<Profile />} />    
+        </Route>
+
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Login from "./pages/Login"
+import Home from "./pages/Home"
+import Profile from "./pages/Profile"
+import Register from "./pages/Register"
+import ResetPassword from "./pages/ResetPassword"
+
+
+
+export default {Login, Home, Profile, Register, ResetPassword};
